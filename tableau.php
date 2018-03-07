a870b35656b4e95a2da6ce9273a719eadcac05ca
Sebastien Petit <sebastien.petit59@laposte.net>
2018-03-07 11:46:25 +0100

    tableau

---
+++ tableau.php
@@ -0,0 +1,39 @@
+<?php
+
+$board = [
+    'Indiana Jones et le Royaume du Crâne de Cristal ',
+    ' Harrison Ford ',
+    ' Cate Blanchett ',
+    ' Karen Allen ', 
+    ];
+
+echo $board[0]; //  Indiana Jones et le Royaume du Crâne de Cristal 
+echo $board[1]; //  Harrison Ford'
+echo $board[2]; //  Cate Blanchett
+echo $board[3]; //  Karen Allen
+echo "</br>";
+
+$board2 = [
+    'Indiana Jones et la Dernière Croisade',
+    ' Harrison Ford ',
+    ' Sean Connery ',
+    ' John Rys-Davies ',
+    ];
+
+echo $board2[0]; // Indiana Jones et la Dernière Croisade
+echo $board2[1]; // Harrison Ford
+echo $board2[2]; //  Sean Connery 
+echo $board2[3]; //   John Rys-Davies
+echo "</br>";
+
+$board3 = [
+    ' Indiana Jones et le Temple maudit  ',
+    ' Harrison Ford ',
+    ' Dan Aykroyd ',
+    ' Amrish Purri ',
+    ];
+
+echo $board2[0]; //  Indiana Jones et le Temple maudit 
+echo $board2[1]; // Harrison Ford
+echo $board2[2]; //  Dan Aykroyd 
+echo $board2[3]; //   Amrish Purri
